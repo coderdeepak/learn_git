3a6d834be601281b764586c265e80536c3a2f7dc
er.deepak16@gmail.com <Welcome@01>
2017-01-30 20:12:46 +0530

    Changes in main by developer 2 again.

--- lean-git-project/src/org/deepak/git/Main.java
+++ lean-git-project/src/org/deepak/git/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// This is going to be the my initial commit by developer1.
 		// Changes in main by developer2.
+		// Changes in main by developer 2 again.
 
 	}
 
