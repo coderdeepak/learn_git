72fe050c8ce51a238393c6d5dda7fba18812c587
Deepak.Keswani <deepak.keswani@synechron.com>
2017-02-02 19:58:35 +0530

    Changed by developer 2 in Branch2

--- lean-git-project/src/org/deepak/git/Main2.java
+++ lean-git-project/src/org/deepak/git/Main2.java
@@ -5,7 +5,7 @@ public class Main2 {
 	public static void main(String[] args) {
 		// Created by developer 2 in branch dev1.
 		// Changed by Devloper2 in branch dev1
-
+		// Changed by Developer 2 in Branch 2
 	}
 
 }
